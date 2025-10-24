6675ae6ef1fe928ccf4d07b61e9076760132f1c9
FihCapua <fihcapua@gmail.com>
2025-10-23 21:43:08 -0300

    feat: create basic HTTP server with example routes

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from 'http';
 
 const PORT = 3000
 
+const routes = {
+    "/": "Curso de Node.js",
+    "/livros": "Entrei na rota livros",
+    "/autores": "Entrei na rota autores"
+}
+
 const server = http.createServer((request, response) => {
     response.writeHead(200, { "Content-Type" : "text/plain" });
-    response.end("Curso de Node.js")
+    response.end(routes[request.url])
 })
 
 server.listen(PORT, () => {
