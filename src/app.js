d8f361f2378da45306fd867b4888032e6f6cb684
FihCapua <fihcapua@gmail.com>
2025-10-26 19:38:19 -0300

    feat: create movie get route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json())
+
+const movies = [
+    {
+        id: 1,
+        titulo: "O Poderoso Chefão"
+    },
+    {
+        id: 2,
+        titulo: "Interstelar"
+    }
+]
 
 app.get('/', (request, response) => {
     response.status(200).send('Curso de Node.js');
+})
+
+app.get('/movies', (request, response) => {
+    response.status(200).json(movies);
 })
 
 export default app;
