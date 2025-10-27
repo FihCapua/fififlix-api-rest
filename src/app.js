b2789fc595fcba448bf29c86181c095fbd45439a
FihCapua <fihcapua@gmail.com>
2025-10-27 20:50:47 -0300

    feat: create search routes and movie updates

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 app.use(express.json())
@@ -6,13 +6,19 @@ app.use(express.json())
 const movies = [
     {
         id: 1,
-        titulo: "O Poderoso Chefão"
+        title: "O Poderoso Chefão"
     },
     {
         id: 2,
-        titulo: "Interstelar"
+        title: "Interstelar"
     }
 ]
+
+const movieSearch = (id) => {
+    return movies.findIndex((movie) => {
+        return movie.id === Number(id)
+    })
+}
 
 app.get('/', (request, response) => {
     response.status(200).send('Curso de Node.js');
@@ -22,9 +28,20 @@ app.get('/movies', (request, response) => {
     response.status(200).json(movies);
 })
 
+app.get('/movies/:id', (request, response) => {
+    const index = movieSearch(request.params.id);
+    response.status(200).json(movies[index])
+})
+
 app.post('/movies', (request, response) => {
     movies.push(request.body);
     response.status(201).send('Filme cadastrado com sucesso.')
+})
+
+app.put('/movies/:id', (request, response) => {
+    const index = movieSearch(request.params.id);
+    movies[index].title = request.body.title;
+    response.status(200).json(movies);
 })
 
 export default app;
